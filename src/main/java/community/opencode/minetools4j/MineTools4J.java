30b837d72227b6bd1d27ec0266f9db7a3447fed4
Skyleiger <skyleiger@gmail.com>
2017-09-24 14:51:04 +0200

    Created main class

---
+++ src/main/java/community/opencode/minetools4j/MineTools4J.java
@@ -0,0 +1,13 @@
+package community.opencode.minetools4j;
+
+public class MineTools4J {
+
+    public static void main(String[] args) {
+        new MineTools4J();
+    }
+
+    public MineTools4J() {
+
+    }
+
+}
